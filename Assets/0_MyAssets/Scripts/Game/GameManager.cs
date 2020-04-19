fc4198af7390b2d6bd69e1a4087fd704790fb41e
家子智成 <>
2020-04-19 20:00:39 +0900

    プレイヤーの大量生成

--- Assets/0_MyAssets/Scripts/Game/GameManager.cs
+++ Assets/0_MyAssets/Scripts/Game/GameManager.cs
@@ -9,15 +9,41 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] PlayerController _playerController;
     [SerializeField] CameraController _cameraController;
+
+    [SerializeField] Transform fieldCornerUR;
+    [SerializeField] Transform fieldCornerLL;
+    [SerializeField] PlayerController wanderingPlayerPrefab;
+    PlayerController[] wanderingPlayers;
+
     void Start()
     {
         _playerController.OnStart();
         _cameraController.OnStart(_playerController.transform.position);
+        WanderingGenerator();
     }
 
     void FixedUpdate()
     {
 
         _cameraController.FollowTarget(_playerController.transform.position);
+    }
+
+    void WanderingGenerator()
+    {
+        wanderingPlayers = new PlayerController[100];
+        for (int i = 0; i < wanderingPlayers.Length; i++)
+        {
+            Vector3 pos = GetRandomPos();
+            wanderingPlayers[i] = Instantiate(wanderingPlayerPrefab, pos, Quaternion.identity, transform);
+            wanderingPlayers[i].OnStart();
+        }
+    }
+
+    public Vector3 GetRandomPos()
+    {
+        //座標をランダムに取得
+        float x = Random.Range(fieldCornerLL.position.x, fieldCornerUR.position.x);
+        float z = Random.Range(fieldCornerLL.position.z, fieldCornerUR.position.z);
+        return new Vector3(x, 0, z);
     }
 }
